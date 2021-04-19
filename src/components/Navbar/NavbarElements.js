403d92efc299fc9f7b2778a50825699964e3ac83
sdgmarton <sdgmarton@gmail.com>
2021-04-19 12:23:45 +0300

    My first Commit

---
+++ src/components/Navbar/NavbarElements.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export const Nav = styled.nav`
+  background: #000;
+  height: 80px;
+  margin-top: -80px;
+  fisplay: flex;
+  justify-content: center;
+  align-items: center;
+  position: sticky
+  
+`
---
+++ src/components/Navbar/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+      <Nav>
+        <NavbarContainer>
+          <NavLogo>
+            mypay
+          </NavLogo>
+        </NavbarContainer>
+      </Nav>
+    </>
+  )
+}
+
+export default Navbar
+
